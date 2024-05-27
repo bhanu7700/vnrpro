bedef413b1537fc9a8a772f99c16ecea8276ae83
bhanu <uprakash.bhanu@leadsnextech.com>
2024-05-27 22:39:19 +0530

    favicon added

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
           rel="stylesheet"
           href="https://unpkg.com/flowbite@1.5.1/dist/flowbite.min.css"
